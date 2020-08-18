84ab171c85552ca992dbdd7452c5f441f45b5696
simylein <67859356+simylein@users.noreply.github.com>
2020-08-18 19:26:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 # Minecraft Server startup script using screen
+
 if screen -list | grep -q "minecraft";then
         echo "Server is already running!  Type screen -r minecraft to open the console"
         exit 1
 fi
+
 NetworkChecks=0
 DefaultRoute=$(route -n | awk '$4 == "UG" {print $2}')
 while [ -z "$DefaultRoute" ]; do
@@ -16,12 +19,17 @@ while [ -z "$DefaultRoute" ]; do
                 break
         fi
 done
+
 echo "Initiated Starting Sequenze for Minecraft Server"
 echo "Starting Minecraft server.  To view window type screen -r minecraft."
 echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
-cd /home/simylein/minecraft/
+
+cd /minecraft/
+
 /usr/bin/screen -dmS minecraft /usr/bin/java -jar -Xms2048 -Xmx8192M /minecraft/paperclip.jar
 echo "Server is Starting..."
 sleep 4s
-cd /home/simylein/
+
+cd
+
 screen -r minecraft
