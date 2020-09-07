616fe3ac9ea3cec3d193f4622270e3b6a59416d5
simylein <67859356+simylein@users.noreply.github.com>
2020-09-07 18:19:59 +0200

    created settings

---
+++ settings.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Minecraft Server settings
+
+# All variables for the server
+servername=$minecraft
+directory=$/home/simylein/
+screen=$/usr/bin/screen
+java=$/usr/bin/java
+mems=$256M
+memx=$2048M
