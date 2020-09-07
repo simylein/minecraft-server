ebfec788ee78aec3b5033936eab3d55f0f7c9f64
simylein <67859356+simylein@users.noreply.github.com>
2020-09-07 20:23:00 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-
 # Minecraft Server update script
 
-cd /home/simylein/minecraft/
+cd ${directory}${servername}
 
-if ! screen -list | grep -q "minecraft"; then
+if ! screen -list | grep -q "${servername}"; then
         echo "Server is not currently running!"
         exit 1
 fi
@@ -12,49 +11,49 @@ fi
 echo "Initiating Updating Sequenze for Minecraft Server"
 
 echo "Server is updating in 30 seconds!"
-screen -Rd minecraft -X stuff "say Server is updating in 30 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 30 seconds! $(printf '\r')"
 sleep 23s
 echo "Server is updating in 7 seconds!"
-screen -Rd minecraft -X stuff "say Server is updating in 7 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 7 seconds! $(printf '\r')"
 sleep 1s
 echo "Server is updating in 6 seconds!"
-screen -Rd minecraft -X stuff "say Server is updating in 6 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 6 seconds! $(printf '\r')"
 sleep 1s
 echo "Server is updating in 5 seconds!"
-screen -Rd minecraft -X stuff "say Server is updating in 5 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 5 seconds! $(printf '\r')"
 sleep 1s
 echo "Server is updating in 4 seconds!"
-screen -Rd minecraft -X stuff "say Server is updating in 4 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 4 seconds! $(printf '\r')"
 sleep 1s
 echo "Server is updating in 3 seconds!"
-screen -Rd minecraft -X stuff "say Server is updating in 3 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 3 seconds! $(printf '\r')"
 sleep 1s
 echo "Server is updating in 2 seconds!"
-screen -Rd minecraft -X stuff "say Server is updating in 2 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 2 seconds! $(printf '\r')"
 sleep 1s
 echo "Server is updating in 1 second!"
-screen -Rd minecraft -X stuff "say Server is updating in 1 second! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 1 second! $(printf '\r')"
 sleep 1s
 echo "Closing server..."
-screen -Rd minecraft -X stuff "say Closing server...$(printf '\r')"
-screen -Rd minecraft -X stuff "stop $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Closing server...$(printf '\r')"
+screen -Rd ${servername} -X stuff "stop $(printf '\r')"
 
 StopChecks=0
 while [ $StopChecks -lt 30 ]; do
-        if ! screen -list | grep -q "minecraft"; then
+        if ! screen -list | grep -q "${servername}"; then
                 break
         fi
         sleep 1;
         StopChecks=$((StopChecks+1))
 done
 
-if screen -list | grep -q "minecraft"; then
+if screen -list | grep -q "${servername}"; then
         echo "Minecraft server still hasn't closed after 30 seconds, closing screen manually"
-        screen -S minecraft -X quit
+        screen -S ${servername} -X quit
 fi
 
-echo "Updating to most recent paperclip version..."
-wget -O /home/simylein/minecraft/paperclip.jar https://papermc.io/api/v1/paper/1.16.2/latest/download
+# Script not finished
+# Placeholder for userinput asking for version - then downloading server file from API - replacing existing one
 
 echo "Restarting Server..."
 ./start.sh
