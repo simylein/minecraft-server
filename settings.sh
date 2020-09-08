7d33cf150e3fd2c8bf3ab769cc89809f454694a8
simylein <67859356+simylein@users.noreply.github.com>
2020-09-08 17:10:04 +0200

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -4,25 +4,25 @@
 # All variables for the server
 
 # Directory path of the screen binary
-screen=$/usr/bin/screen
+screen='/usr/bin/screen'
 
 # Directory path of the java binary
-java=$/usr/bin/java
+java='/usr/bin/java'
 
 # Directory path of the server.jar
-serverfile=$/minecraft_server.1.16.2.jar
+serverfile='/minecraft_server.1.16.2.jar'
 
 # Name of Server
-servername=$minecraft
+servername='minecraft'
 
 # Homedirectory
-directory=$/home/simylein/
+directory='/home/simylein/'
 
 # Minimum ammount of Memory granted
-mems=$-Xms256M
+mems='-Xms256M'
 
 # Maximum ammount of Memory granted
-memx=$-Xmx2048M
+memx='-Xmx2048M'
 
 # Number of Threads allowed
-threadcount=$-XX:ParallelGCThreads=2
+threadcount='-XX:ParallelGCThreads=2'
