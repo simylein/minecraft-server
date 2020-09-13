9bce4df906672c739eb9cabc602aac8e2be74a1b
simylein <67859356+simylein@users.noreply.github.com>
2020-09-13 11:21:48 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -3,7 +3,7 @@
 
 . ./settings.sh
 
-cd ${directory}${servername}
+cd ${serverdirectory}
 
 if ! screen -list | grep -q "${servername}"; then
         echo "Server is not currently running!"
@@ -13,32 +13,32 @@ fi
 echo "Initiating Stopping Sequenze for Minecraft Server"
 
 echo "Server is stopping in 30 seconds!"
-screen -Rd ${servername} -X stuff "say Server is stopping in 30 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is stopping in 30 seconds!$(printf '\r')"
 sleep 23s
 echo "Server is stopping in 7 seconds!"
-screen -Rd ${servername} -X stuff "say Server is stopping in 7 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is stopping in 7 seconds!$(printf '\r')"
 sleep 1s
 echo "Server is stopping in 6 seconds!"
-screen -Rd ${servername} -X stuff "say Server is stopping in 6 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is stopping in 6 seconds!$(printf '\r')"
 sleep 1s
 echo "Server is stopping in 5 seconds!"
-screen -Rd ${servername} -X stuff "say Server is stopping in 5 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is stopping in 5 seconds!$(printf '\r')"
 sleep 1s
 echo "Server is stopping in 4 seconds!"
-screen -Rd ${servername} -X stuff "say Server is stopping in 4 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is stopping in 4 seconds!$(printf '\r')"
 sleep 1s
 echo "Server is stopping in 3 seconds!"
-screen -Rd ${servername} -X stuff "say Server is stopping in 3 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is stopping in 3 seconds!$(printf '\r')"
 sleep 1s
 echo "Server is stopping in 2 seconds!"
-screen -Rd ${servername} -X stuff "say Server is stopping in 2 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is stopping in 2 seconds!$(printf '\r')"
 sleep 1s
 echo "Server is stopping in 1 second!"
-screen -Rd ${servername} -X stuff "say Server is stopping in 1 second! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is stopping in 1 second!$(printf '\r')"
 sleep 1s
 echo "Stopping server..."
 screen -Rd ${servername} -X stuff "say Stopping server...$(printf '\r')"
-screen -Rd ${servername} -X stuff "stop $(printf '\r')"
+screen -Rd ${servername} -X stuff "stop$(printf '\r')"
 
 StopChecks=0
 while [ $StopChecks -lt 30 ]; do
