c888798ab57b92e650c8920831eeebf438087b47
simylein <67859356+simylein@users.noreply.github.com>
2020-09-13 11:25:43 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -5,12 +5,14 @@
 
 echo "Starting Backup..."
 echo "Backing up Server Files for Minecraft Server... (This may take a while)"
+screen -Rd ${servername} -X stuff "Complete Server Database Backup has started...$(printf '\r')"
 
 date +"%Y-%m-%d"
 today=$(date +"%Y-%m-%d")
 
 echo "Backup Location as follows "${directory}${servername}/${servername}${today}"
 
-cp -r ${directory}${servername} ${directory}${servername}/${servername}-${today}
+cp -r ${serverdirectory} ${serverdirectory}${servername}-${today}
 
 echo "Server Backups finished Succesfully"
+screen -Rd ${servername} -X stuff "Complete Server Database Backup has successful finished!$(printf '\r')"
