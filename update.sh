76630611a1f4dcdad6cfdeda8edbd83eb00bf971
simylein <67859356+simylein@users.noreply.github.com>
2020-09-13 11:27:30 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 
 . ./settings.sh
 
-cd ${directory}${servername}
+cd ${serverdirectory}
 
 if ! screen -list | grep -q "${servername}"; then
         echo "Server is not currently running!"
@@ -13,32 +13,32 @@ fi
 echo "Initiating Updating Sequenze for Minecraft Server"
 
 echo "Server is updating in 30 seconds!"
-screen -Rd ${servername} -X stuff "say Server is updating in 30 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 30 seconds!$(printf '\r')"
 sleep 23s
 echo "Server is updating in 7 seconds!"
-screen -Rd ${servername} -X stuff "say Server is updating in 7 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 7 seconds!$(printf '\r')"
 sleep 1s
 echo "Server is updating in 6 seconds!"
-screen -Rd ${servername} -X stuff "say Server is updating in 6 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 6 seconds!$(printf '\r')"
 sleep 1s
 echo "Server is updating in 5 seconds!"
-screen -Rd ${servername} -X stuff "say Server is updating in 5 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 5 seconds!$(printf '\r')"
 sleep 1s
 echo "Server is updating in 4 seconds!"
-screen -Rd ${servername} -X stuff "say Server is updating in 4 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 4 seconds!$(printf '\r')"
 sleep 1s
 echo "Server is updating in 3 seconds!"
-screen -Rd ${servername} -X stuff "say Server is updating in 3 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 3 seconds!$(printf '\r')"
 sleep 1s
 echo "Server is updating in 2 seconds!"
-screen -Rd ${servername} -X stuff "say Server is updating in 2 seconds! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 2 seconds!$(printf '\r')"
 sleep 1s
 echo "Server is updating in 1 second!"
-screen -Rd ${servername} -X stuff "say Server is updating in 1 second! $(printf '\r')"
+screen -Rd ${servername} -X stuff "say Server is updating in 1 second!$(printf '\r')"
 sleep 1s
 echo "Closing server..."
 screen -Rd ${servername} -X stuff "say Closing server...$(printf '\r')"
-screen -Rd ${servername} -X stuff "stop $(printf '\r')"
+screen -Rd ${servername} -X stuff "stop$(printf '\r')"
 
 StopChecks=0
 while [ $StopChecks -lt 30 ]; do
@@ -56,6 +56,7 @@ fi
 
 # Script not finished
 # Placeholder for userinput asking for version - then downloading server file from API - replacing existing one
+sleep 600s
 
 echo "Restarting Server..."
 ./start.sh
