a116288f0057eb91191eddecf542d7b315492e92
simylein <67859356+simylein@users.noreply.github.com>
2020-09-24 09:15:23 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # Script for setting up a Minecraft Server
 
-# Feel free to change those variables to somthing you like
-servername='minecraft'
-homedirectory='/home/simylein/'
+echo "How should I call your Server?"
+read -p "Please enter a servername: " servername
+echo "Your Server will be called $servername"
+
+homedirectory=`pwd`
 
 echo "I will now setup a server and backup directory. "
 echo "I will also download the following scripts:"
 echo "start, stop, restart, backup, update, maintenance, speedrun and varo."
 echo "Continue?"
-read -p "[Y] or [N]:"
+read -p "[Y/N]:"
 if [[ $REPLY =~ ^[Yy]$ ]]
 	then echo "starting setup..."
 	else echo "exiting..."
@@ -21,15 +23,15 @@ mkdir ${servername}
 
 echo "Downloading Scripts from GitHub..."
 	cd ${servername}
-		wget -O settings.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/settings.sh
-		wget -O start.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/start.sh
-		wget -O restart.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/restart.sh
-		wget -O stop.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/stop.sh
-		wget -O backup.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/backup.sh
-		wget -O update.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/update.sh
-		wget -O maintenance.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/maintenance.sh
-		wget -O speedrun.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/speedrun.sh
-		wget -O varo.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/varo.sh
+		wget -q -O settings.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/settings.sh
+		wget -q -O start.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/start.sh
+		wget -q -O restart.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/restart.sh
+		wget -q -O stop.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/stop.sh
+		wget -q -O backup.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/backup.sh
+		wget -q -O update.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/update.sh
+		wget -q -O maintenance.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/maintenance.sh
+		wget -q -O speedrun.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/speedrun.sh
+		wget -q -O varo.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/varo.sh
 
 echo "Making Scripts executable..."
 		chmod +x start.sh
