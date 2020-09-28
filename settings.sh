adc3b46651785d9206401939181b456abdc2332a
simylein <67859356+simylein@users.noreply.github.com>
2020-09-28 20:27:24 +0200

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -4,13 +4,11 @@
 # This file stores all the variables for the server. 
 # If you know what you are doing, feel free to tinker with them ;^)
 
+# command line colors
 red='\033[0;31m'
 yellow='\033[1;33m'
 green='\033[0;32m'
 blue='\033[0;34m'
 purple='\033[0;35m'
 nocolor='\033[0m'
-screen='/usr/bin/screen'
-java='/usr/bin/java'
-dnsserver='1.1.1.1'
-interface='192.168.1.1'
+# server variables
