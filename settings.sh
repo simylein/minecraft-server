1f56a69a0e41a2e232eb99dfcba1308e9b8e246d
simylein <67859356+simylein@users.noreply.github.com>
2020-09-28 20:37:37 +0200

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -5,10 +5,10 @@
 # If you know what you are doing, feel free to tinker with them ;^)
 
 # command line colors
-red='\033[0;31m'
-yellow='\033[1;33m'
-green='\033[0;32m'
-blue='\033[0;34m'
-purple='\033[0;35m'
-nocolor='\033[0m'
+red="\033[0;31m"
+yellow="\033[1;33m"
+green="\033[0;32m"
+blue="\033[0;34m"
+purple="\033[0;35m"
+nocolor="\033[0m"
 # server variables
