6374c1297505ab2175730d8c687416f9ba2531ae
simylein <67859356+simylein@users.noreply.github.com>
2020-10-01 20:24:37 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,43 +1,35 @@
 #!/bin/sh
 # Minecraft Server restart script
 
+# read the settings
 . ./settings.sh
 
+# change to server directory
 cd ${serverdirectory}
 
+# check if server is running
 if ! screen -list | grep -q "${servername}"; then
         echo -e "${yellow}Server is not currently running!${nocolor}"
         exit 1
 fi
 
-echo "Server is restarting in 30 seconds!"
-screen -Rd ${servername} -X stuff "say Server is restarting in 30 seconds!$(printf '\r')"
-sleep 23s
-echo "Server is restarting in 7 seconds!"
-screen -Rd ${servername} -X stuff "say Server is restarting in 7 seconds!$(printf '\r')"
+# countdown
+counter="60"
+while [ ${counter} -gt 0 ]; do
+        if [[ "${counter}" =~ ^(60|40|20|10|5|4|3|2|1)$ ]];then
+                echo "server is restarting in ${counter} seconds!"
+                screen -Rd ${servername} -X stuff "say server is restarting in ${counter} seconds!$(printf '\r')"
+        fi
+counter=$((counter-1))
 sleep 1s
-echo "Server is restarting in 6 seconds!"
-screen -Rd ${servername} -X stuff "say Server is restarting in 6 seconds!$(printf '\r')"
-sleep 1s
-echo "Server is restarting in 5 seconds!"
-screen -Rd ${servername} -X stuff "say Server is restarting in 5 seconds!$(printf '\r')"
-sleep 1s
-echo "Server is restarting in 4 seconds!"
-screen -Rd ${servername} -X stuff "say Server is restarting in 4 seconds!$(printf '\r')"
-sleep 1s
-echo "Server is restarting in 3 seconds!"
-screen -Rd ${servername} -X stuff "say Server is restarting in 3 seconds!$(printf '\r')"
-sleep 1s
-echo "Server is restarting in 2 seconds!"
-screen -Rd ${servername} -X stuff "say Server is restarting in 2 seconds!$(printf '\r')"
-sleep 1s
-echo "Server is restarting in 1 second!"
-screen -Rd ${servername} -X stuff "say Server is restarting in 1 second!$(printf '\r')"
-sleep 1s
-echo "Closing server..."
-screen -Rd ${servername} -X stuff "say Closing server...$(printf '\r')"
+done
+
+# server stop
+echo "stopping server..."
+screen -Rd ${servername} -X stuff "say stopping server...$(printf '\r')"
 screen -Rd ${servername} -X stuff "stop$(printf '\r')"
 
+# check if server stopped
 StopChecks=0
 while [ $StopChecks -lt 30 ]; do
         if ! screen -list | grep -q "${servername}"; then
@@ -47,10 +39,12 @@ while [ $StopChecks -lt 30 ]; do
         StopChecks=$((StopChecks+1))
 done
 
+# force quit server if not stopped
 if screen -list | grep -q "${servername}"; then
         echo -e "${yellow}Minecraft server still hasn't closed after 30 seconds, closing screen manually${nocolor}"
         screen -S ${servername} -X quit
 fi
 
-echo -e "${green}restarting Server...${nocolor}"
+# restart the server
+echo -e "${green}restarting server...${nocolor}"
 ./start.sh
