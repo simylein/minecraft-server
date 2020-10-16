f1bf1596e2538fe9dbbaea9046fdc9bf48ce74d8
simylein <67859356+simylein@users.noreply.github.com>
2020-10-16 11:16:28 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,21 +8,21 @@
 cd ${serverdirectory}
 
 # adding new backup
-echo "creating new backup..."
+echo -e "${blue}creating new backup...${nocolor}"
 screen -Rd ${servername} -X stuff "say creating new backup...$(printf '\r')"
 
 cp -r ${serverdirectory} ${backupdirectory}/${servername}-${new}
 
 # output file location of new backup
-echo "file available under ${backupdirectory}/${servername}-${new}"
+echo -e "${blue}file available under ${backupdirectory}/${servername}-${new}${nocolor}"
 screen -Rd ${servername} -X stuff "say file available under ${backupdirectory}/${servername}-${new}$(printf '\r')"
 
 # deleting old backup
-echo "deleting old backup..."
+echo -e "${red}deleting old backup...${nocolor}"
 screen -Rd ${servername} -X stuff "say deleting old backup...$(printf '\r')"
 
 rm -r ${backupdirectory}/${servername}-${old}
 
 # output deleted backup location
-echo "deleted ${backupdirectory}/${servername}-${old}"
+echo -e "${red}deleted ${backupdirectory}/${servername}-${old}${nocolor}"
 screen -Rd ${servername} -X stuff "say deleted ${backupdirectory}/${servername}-${old}$(printf '\r')"
