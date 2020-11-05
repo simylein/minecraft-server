46370e228f7a7cf0137fdb2a27a9e7c16d2f5d70
simylein <67859356+simylein@users.noreply.github.com>
2020-11-05 14:50:03 +0100

    better timestamps in crontab

--- setup.sh
+++ setup.sh
@@ -273,10 +273,10 @@ echo "storing variables in server.properties..."
 read -p "Would you like to automate backups? [Y/N]:"
 if [[ $REPLY =~ ^[Yy]$ ]]
 	then echo -e "${green}automating backups...${nocolor}"
-		crontab -l | { cat; echo "# minecraft ${servername} server backup hourly"; } | crontab -
-		crontab -l | { cat; echo "0 * * * * cd ${serverdirectory} && ${serverdirectory}/backuphourly.sh"; } | crontab -
+		crontab -l | { cat; echo "# minecraft ${servername} server backup hourly at **:00"; } | crontab -
+		crontab -l | { cat; echo "00 * * * * cd ${serverdirectory} && ${serverdirectory}/backuphourly.sh"; } | crontab -
 		crontab -l | { cat; echo "# minecraft ${servername} server backup daily at 22:00"; } | crontab -
-		crontab -l | { cat; echo "0 22 * * * cd ${serverdirectory} && ${serverdirectory}/backupdaily.sh"; } | crontab -
+		crontab -l | { cat; echo "00 22 * * * cd ${serverdirectory} && ${serverdirectory}/backupdaily.sh"; } | crontab -
 	else echo -e "${red}no automated backups...${nocolor}"
 fi
 
