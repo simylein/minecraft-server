beea608d4a6bfd2dfc426e8c58feca7549b82f53
simylein <67859356+simylein@users.noreply.github.com>
2020-12-10 16:54:26 +0100

    better code layout

--- update.sh
+++ update.sh
@@ -56,10 +56,10 @@ if [ "$?" != 0 ]; then
 	else
 	echo -e "${green}downloading newest server version...${nocolor}"
 	echo "downloading newest server version..." >> ${screenlog}
-	wget -q -O minecraft-server.1.16.4.jar https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar
-	# update serverfile variable in server.settings
-	newserverfile=${serverdirectory}"/minecraft-server.1.16.4.jar"
-	sed -i "s|$serverfile|$newserverfile|g" server.settings
+		wget -q -O minecraft-server.1.16.4.jar https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar
+		# update serverfile variable in server.settings
+		newserverfile=${serverdirectory}"/minecraft-server.1.16.4.jar"
+		sed -i "s|$serverfile|$newserverfile|g" server.settings
 fi
 
 # Test internet connectivity and update on success
@@ -67,42 +67,42 @@ wget --spider --quiet https://raw.githubusercontent.com/Simylein/MinecraftServer
 if [ "$?" != 0 ]; then
 	echo -e "${red}Warning: Unable to connect to GitHub API. Skipping update ...${nocolor}"
 	echo "Warning: Unable to connect to GitHub API. Skipping update ..." >> ${screenlog}
-else
+	else
 	echo -e "${green}downloading newest scripts version...${nocolor}"
 	echo "downloading newest scripts version..." >> ${screenlog}
-	# remove all scripts
-	rm LICENSE
-	rm README.md
-	rm start.sh
-	rm restore.sh
-	rm reset.sh
-	rm restart.sh
-	rm stop.sh
-	rm backuphourly.sh
-	rm backupdaily.sh
-	rm update.sh
-	rm maintenance.sh
-	# download all scripts
-	wget -q -O LICENSE https://raw.githubusercontent.com/Simylein/MinecraftServer/master/LICENSE
-	wget -q -O README.md https://raw.githubusercontent.com/Simylein/MinecraftServer/master/README.md
-	wget -q -O start.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/start.sh
-	wget -q -O restore.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/restore.sh
-	wget -q -O reset.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/reset.sh
-	wget -q -O restart.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/restart.sh
-	wget -q -O stop.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/stop.sh
-	wget -q -O backuphourly.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/backuphourly.sh
-	wget -q -O backupdaily.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/backupdaily.sh
-	wget -q -O update.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/update.sh
-	wget -q -O maintenance.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/maintenance.sh
-	# make scripts executable
-	chmod +x start.sh
-	chmod +x restart.sh
-	chmod +x restore.sh
-	chmod +x stop.sh
-	chmod +x update.sh
-	chmod +x maintenance.sh
-	chmod +x backuphourly.sh
-	chmod +x backupdaily.sh
+		# remove all scripts
+		rm LICENSE
+		rm README.md
+		rm start.sh
+		rm restore.sh
+		rm reset.sh
+		rm restart.sh
+		rm stop.sh
+		rm backuphourly.sh
+		rm backupdaily.sh
+		rm update.sh
+		rm maintenance.sh
+		# download all scripts
+		wget -q -O LICENSE https://raw.githubusercontent.com/Simylein/MinecraftServer/master/LICENSE
+		wget -q -O README.md https://raw.githubusercontent.com/Simylein/MinecraftServer/master/README.md
+		wget -q -O start.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/start.sh
+		wget -q -O restore.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/restore.sh
+		wget -q -O reset.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/reset.sh
+		wget -q -O restart.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/restart.sh
+		wget -q -O stop.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/stop.sh
+		wget -q -O backuphourly.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/backuphourly.sh
+		wget -q -O backupdaily.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/backupdaily.sh
+		wget -q -O update.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/update.sh
+		wget -q -O maintenance.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/maintenance.sh
+		# make scripts executable
+		chmod +x start.sh
+		chmod +x restart.sh
+		chmod +x restore.sh
+		chmod +x stop.sh
+		chmod +x update.sh
+		chmod +x maintenance.sh
+		chmod +x backuphourly.sh
+		chmod +x backupdaily.sh
 fi
 
 # restart the server
