0cb79af10888e72080743a209e7c69f2a5062dbe
simylein <67859356+simylein@users.noreply.github.com>
2020-12-29 21:40:47 +0100

    added ingame colors in chat

--- backupdaily.sh
+++ backupdaily.sh
@@ -7,51 +7,41 @@
 # change to server directory
 cd ${serverdirectory}
 
+# get current world size
+worldsize=$(du -sh world | cut -f1)
+
 # write date to logfiles
 echo "${date} executing backup-daily script" >> ${screenlog}
 echo "${date} executing backup-daily script" >> ${backuplog}
 
 # check if server is running
 if ! screen -list | grep -q "${servername}"; then
-	echo -e "${yellow}Server is not currently running!${nocolor}"
-	echo "Server is not currently running!" >> ${screenlog}
+	echo -e "${yellow}server is not currently running!${nocolor}"
+	echo "server is not currently running!" >> ${screenlog}
 	exit 1
 fi
 
-# check if there is no backup from today
+# check if there is no backup from the current day
 if ! [ -d "${backupdirectory}/daily/${servername}-${newdaily}" ]; then
 	cp -r ${serverdirectory}/world ${backupdirectory}/daily/${servername}-${newdaily}
 else
-	echo "warning backup already exists!" >> ${backuplog}
+	echo "warning: backup already exists!" >> ${backuplog}
 	exit 1
 fi
 
-# ingame and logfile success and error output
-if [ -d "${backupdirectory}/daily/${servername}-${newdaily}" ]; then
-	screen -Rd ${servername} -X stuff "say newest backup has been successfully created!$(printf '\r')"
-	echo "newest backup has been successfully created!" >> ${backuplog}
-	echo "added ${backupdirectory}/daily/${servername}-${newdaily}" >> ${backuplog}
-else
-	screen -Rd ${servername} -X stuff "say fatal could not backup world - please tell your server admin!$(printf '\r')"
-	echo "fatal could not backup world!" >> ${backuplog}
-fi
-
-# check if there is a backup from today
+# check if there is a new daily backup and output colorful success and error messages to ingame chat
 if [ -d "${backupdirectory}/daily/${servername}-${newdaily}" ]; then
 	rm -r ${backupdirectory}/daily/${servername}-${olddaily}
-else
-	echo "warning cannot remove old backup because new backup is missing" >> ${backuplog}
-	exit 1
-fi
-
-# ingame and logfile success and error output
-if ! [ -d "${backupdirectory}/daily/${servername}-${olddaily}" ]; then
-	screen -Rd ${servername} -X stuff "say oldest backup has been successfully removed!$(printf '\r')"
+	screen -Rd ${servername} -X stuff "tellraw @a [\"\",{\"text\":\"[Backup] \",\"color\":\"gray\",\"italic\":true},{\"text\":\"successfully created new backup\",\"color\":\"green\",\"italic\":true,\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"created file: ${servername}-${newdaily}, removed file: ${servername}-${olddaily, current world size: ${worldsize}\"}]}}}]$(printf '\r')"
+	echo "newest backup has been successfully created!" >> ${backuplog}
+	echo "added ${backupdirectory}/daily/${servername}-${newdaily}" >> ${backuplog}
 	echo "oldest backup has been successfully removed!" >> ${backuplog}
 	echo "removed ${backupdirectory}/daily/${servername}-${olddaily}" >> ${backuplog}
 else
-	screen -Rd ${servername} -X stuff "say warning could not remove old backup - please tell your server admin!$(printf '\r')"
-	echo "warning could not remove old backup!" >> ${backuplog}
+	screen -Rd ${servername} -X stuff "tellraw @a [\"\",{\"text\":\"[Backup] \",\"color\":\"gray\",\"italic\":true},{\"text\":\"fatal: could not create new backup - please tell your server admin\",\"color\":\"red\",\"italic\":true,\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"could not create file: ${servername}-${newdaily}, could not remove file: ${servername}-${olddaily}, current world size: ${worldsize}\"}]}}}]$(printf '\r')"
+	echo "warning: cannot remove old backup because new backup is missing" >> ${backuplog}
+	echo "warning: could not remove old backup!" >> ${backuplog}
+	echo "fatal: could not backup world!" >> ${backuplog}
 fi
 
 # write one padding line to backuplog
--- backuphourly.sh
+++ backuphourly.sh
@@ -7,51 +7,41 @@
 # change to server directory
 cd ${serverdirectory}
 
+# get current world size
+worldsize=$(du -sh world | cut -f1)
+
 # write date to logfiles
 echo "${date} executing backup-hourly script" >> ${screenlog}
 echo "${date} executing backup-hourly script" >> ${backuplog}
 
 # check if server is running
 if ! screen -list | grep -q "${servername}"; then
-	echo -e "${yellow}Server is not currently running!${nocolor}"
-	echo "Server is not currently running!" >> ${screenlog}
+	echo -e "${yellow}server is not currently running!${nocolor}"
+	echo "server is not currently running!" >> ${screenlog}
 	exit 1
 fi
 
-# check if there is no backup from today
+# check if there is no backup from the current hour
 if ! [ -d "${backupdirectory}/hourly/${servername}-${newhourly}" ]; then
 	cp -r ${serverdirectory}/world ${backupdirectory}/hourly/${servername}-${newhourly}
 else
-	echo "warning backup already exists!" >> ${backuplog}
+	echo "warning: backup already exists!" >> ${backuplog}
 	exit 1
 fi
 
-# ingame and logfile success and error output
-if [ -d "${backupdirectory}/hourly/${servername}-${newhourly}" ]; then
-	screen -Rd ${servername} -X stuff "say newest backup has been successfully created!$(printf '\r')"
-	echo "newest backup has been successfully created!" >> ${backuplog}
-	echo "added ${backupdirectory}/hourly/${servername}-${newhourly}" >> ${backuplog}
-else
-	screen -Rd ${servername} -X stuff "say fatal could not backup world - please tell your server admin!$(printf '\r')"
-	echo "fatal could not backup world!" >> ${backuplog}
-fi
-
-# check if there is a backup from today
+# check if there is a new hourly backup and output colorful success and error messages to ingame chat
 if [ -d "${backupdirectory}/hourly/${servername}-${newhourly}" ]; then
 	rm -r ${backupdirectory}/hourly/${servername}-${oldhourly}
-else
-	echo "warning cannot remove old backup because new backup is missing" >> ${backuplog}
-	exit 1
-fi
-
-# ingame and logfile success and error output
-if ! [ -d "${backupdirectory}/hourly/${servername}-${oldhourly}" ]; then
-	screen -Rd ${servername} -X stuff "say oldest backup has been successfully removed!$(printf '\r')"
+	screen -Rd ${servername} -X stuff "tellraw @a [\"\",{\"text\":\"[Backup] \",\"color\":\"gray\",\"italic\":true},{\"text\":\"successfully created new backup\",\"color\":\"green\",\"italic\":true,\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"created file: ${servername}-${newhourly}, removed file: ${servername}-${oldhourly}, current world size: ${worldsize}\"}]}}}]$(printf '\r')"
+	echo "newest backup has been successfully created!" >> ${backuplog}
+	echo "added ${backupdirectory}/hourly/${servername}-${newhourly}" >> ${backuplog}
 	echo "oldest backup has been successfully removed!" >> ${backuplog}
 	echo "removed ${backupdirectory}/hourly/${servername}-${oldhourly}" >> ${backuplog}
 else
-	screen -Rd ${servername} -X stuff "say warning could not remove old backup - please tell your server admin!$(printf '\r')"
-	echo "warning could not remove old backup!" >> ${backuplog}
+	screen -Rd ${servername} -X stuff "tellraw @a [\"\",{\"text\":\"[Backup] \",\"color\":\"gray\",\"italic\":true},{\"text\":\"fatal: could not create new backup - please tell your server admin\",\"color\":\"red\",\"italic\":true,\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"could not create file: ${servername}-${newhourly}, could not remove file: ${servername}-${oldhourly}, current world size: ${worldsize}\"}]}}}]$(printf '\r')"
+	echo "warning: cannot remove old backup because new backup is missing" >> ${backuplog}
+	echo "warning: could not remove old backup!" >> ${backuplog}
+	echo "fatal: could not backup world!" >> ${backuplog}
 fi
 
 # write one padding line to backuplog
