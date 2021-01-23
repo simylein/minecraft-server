6f60b87877a9d7fa8daa4ba3d30da3a7b8cc4c0b
simylein <67859356+simylein@users.noreply.github.com>
2021-01-23 17:54:05 +0100

    added include file with error checking

--- prerender.sh
+++ prerender.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
 # mincraft server prerender script
 
-# read the settings
-. ./server.settings
+# read server.functions file with error checking
+if [[ -f "server.functions" ]]; then
+	. ./server.functions
+else
+	echo "fatal: server.functions is missing" >> fatalerror.log
+	echo "fatal: server.functions is missing"
+	exit 1
+fi
+
+# read server.properties file with error checking
+if [[ -f "server.properties" ]]; then
+else
+	echo "fatal: server.properties is missing" >> fatalerror.log
+	echo "fatal: server.properties is missing"
+	exit 1
+fi
+
+# read server.settings file with error checking
+if [[ -f "server.settings" ]]; then
+	. ./server.settings
+else
+	echo "fatal: server.settings is missing" >> fatalerror.log
+	echo "fatal: server.settings is missing"
+	exit 1
+fi
+
+# change to server directory with error checking
+if [ -d "${serverdirectory}" ]; then
+	cd ${serverdirectory}
+else
+	echo "fatal: serverdirectory is missing" >> fatalerror.log
+	echo "fatal: serverdirectory is missing"
+	exit 1
+fi
 
 cords=( 2048 1792 1536 1280 1024 768 512 256 0 -256 -512 -768 -1024 -1280 -1536 -1792 -2048)
-
-# change to server directory
-cd ${serverdirectory}
 
 # check if server is running
 if ! screen -list | grep -q "${servername}"; then
