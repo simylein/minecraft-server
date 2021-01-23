7d2892ea44b43218ae959d18858e164258005dc2
simylein <67859356+simylein@users.noreply.github.com>
2021-01-23 18:00:48 +0100

    added logging for safety backup

--- update.sh
+++ update.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
 # minecraft server update script
 
-# read the settings
-. ./server.settings
+# read server.functions file with error checking
+if [[ -f "server.functions" ]]; then
+	. ./server.functions
+else
+	echo "fatal: server.functions is missing" >> fatalerror.log
+	echo "fatal: server.functions is missing"
+	exit 1
+fi
 
-# change to serverdirectory
-cd ${serverdirectory}
+# read server.properties file with error checking
+if [[ -f "server.properties" ]]; then
+else
+	echo "fatal: server.properties is missing" >> fatalerror.log
+	echo "fatal: server.properties is missing"
+	exit 1
+fi
+
+# read server.settings file with error checking
+if [[ -f "server.settings" ]]; then
+	. ./server.settings
+else
+	echo "fatal: server.settings is missing" >> fatalerror.log
+	echo "fatal: server.settings is missing"
+	exit 1
+fi
+
+# change to server directory with error checking
+if [ -d "${serverdirectory}" ]; then
+	cd ${serverdirectory}
+else
+	echo "fatal: serverdirectory is missing" >> fatalerror.log
+	echo "fatal: serverdirectory is missing"
+	exit 1
+fi
 
 # write date to logfile
 echo "${date} executing update script" >> ${screenlog}
 
 # check if server is running
 if ! screen -list | grep -q "${servername}"; then
-		echo -e "${yellow}Server is not currently running!${nocolor}"
-		exit 1
+	echo "server is not currently running!" >> ${screenlog}
+	echo -e "${yellow}server is not currently running!${nocolor}"
+	exit 1
 fi
 
 # countdown
@@ -33,14 +63,7 @@ screen -Rd ${servername} -X stuff "say stopping server...$(printf '\r')"
 screen -Rd ${servername} -X stuff "stop$(printf '\r')"
 
 # check if server stopped
-stopchecks="0"
-while [ $stopchecks -lt 30 ]; do
-		if ! screen -list | grep -q "${servername}"; then
-				break
-		fi
-stopchecks=$((stopchecks+1))
-sleep 1;
-done
+CheckForStop
 
 # force quit server if not stopped
 if screen -list | grep -q "${servername}"; then
@@ -56,6 +79,8 @@ cp -r ${serverdirectory}/world ${backupdirectory}/cached/update-${newdaily}
 if ! [ -d "${backupdirectory}/cached/update-${newdaily}" ]; then
 	echo -e "${red}warning: safety backup failed - proceeding to server update${nocolor}"
 	echo "warning: safety backup failed - proceeding to server update" >> ${screenlog}
+else
+	echo "created ${backupdirectory}/cached/update-${newdaily} as a safety backup" >> ${backuplog}
 fi
 
 # Test internet connectivity and update on success
