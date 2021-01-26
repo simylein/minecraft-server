f512a82f6486bdce208f0d14b2265dbd75259b5f
simylein <67859356+simylein@users.noreply.github.com>
2021-01-26 12:52:07 +0100

    added vent.sh to download

--- setup.sh
+++ setup.sh
@@ -52,6 +52,7 @@ echo "downloading scripts from GitHub..."
 		wget -q -O backup.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/backup.sh && chmod +x backup.sh
 		wget -q -O update.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/update.sh && chmod +x update.sh
 		wget -q -O maintenance.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/maintenance.sh && chmod +x maintenance.sh
+		wget -q -O vent.sh https://raw.githubusercontent.com/Simylein/MinecraftServer/master/vent.sh
 
 # store serverdirectory
 serverdirectory=`pwd`
