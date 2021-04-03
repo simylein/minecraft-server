3211faf740baebe4d9004a92de99548f74877909
simylein <67859356+simylein@users.noreply.github.com>
2021-04-03 16:34:07 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -69,7 +69,7 @@ if ! [[ ${immediatly} == true ]]; then
 fi
 
 # server stop
-CheckVerbose "stopping server..."
+CheckQuiet "stopping server..."
 PrintToScreen "say stopping server..."
 PrintToScreen "stop"
 
